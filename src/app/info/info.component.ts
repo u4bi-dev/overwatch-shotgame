9c942e2b1cb7788e81b449bf8436ac2a8f27bd12
u4bi <u4bi@u4bi.com>
2017-02-09 02:51:50 +0900

    인포 뷰단 프로토타입 렌더 v.001

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MdSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-info',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private snackbar: MdSnackBar) { }
 
   ngOnInit() {
+  }
+
+  showMaxrecord() {
+      this.snackbar.open('당신의 최고 기록 시간은 39.54초 입니다.', '확인');
   }
 
 }
