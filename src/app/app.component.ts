91699c6a068e6fea8bcaef346b81318b067471c7
u4bi <u4bi@u4bi.com>
2017-02-10 01:00:02 +0900

    로그인/로그아웃 토클 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,24 @@ import {MdSidenav, MdSnackBar} from '@angular/material';
 })
 export class AppComponent {
   @ViewChild('sidenav') sidenav:MdSidenav;
+  private isLogin: boolean;
 
   constructor(
     private appCoreService : AppCoreService,
     private appFirebaseService : AppFirebaseService,
     private snackbar: MdSnackBar
-  ){}
+  ){
+    this.appFirebaseService.firebase.auth.subscribe(
+      (auth) => {
+        if(auth == null){
+          this.isLogin = false;
+        }
+        else {
+          this.isLogin = true;
+        }
+      }
+    );
+  }
 
   setRouter(path : string){
     this.sidenav.close();
@@ -27,7 +39,11 @@ export class AppComponent {
     this.appFirebaseService.loginGoogle().then((data) =>{
       this.snackbar.open(data.auth.displayName+'님 반갑습니다. 로그인에 성공하였습니다.', '확인',{ duration: 3000});
     });
+  }
 
+  logout(){
+    this.appFirebaseService.logout();
+    this.snackbar.open('성공적으로 로그아웃이 되었습니다.', '확인',{ duration: 3000});
   }
 
 }
