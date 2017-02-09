2bdd9b222525777a5d53ff86047d175a3e41b0cf
u4bi <u4bi@u4bi.com>
2017-02-09 13:28:12 +0900

    인포 스네이크바 추가 - 기존 : showMaxrecord()   @ 나의 최고기록 보기 - 추가사항 : showInfo()   @ 내 정보 보기       - 닉네임       - 레벨       - 경험치

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -7,14 +7,17 @@ import {MdSnackBar} from '@angular/material';
   styleUrls: ['./info.component.css']
 })
 export class InfoComponent implements OnInit {
-
+  
   constructor(private snackbar: MdSnackBar) { }
 
   ngOnInit() {
   }
 
   showMaxrecord() {
-      this.snackbar.open('당신의 최고 기록 시간은 39.54초 입니다.', '확인');
+      this.snackbar.open('당신의 최고 기록 시간은 39.54초 입니다.', '확인',{ duration: 1500});
   }
 
+  showInfo() {
+      this.snackbar.open('닉네임 홍길동 레벨 24 경험치 32/100', '확인',{ duration: 3000});
+  }
 }
