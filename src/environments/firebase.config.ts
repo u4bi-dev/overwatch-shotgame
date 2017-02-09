19be20e80565fafbcbcc79b159d60ba1afcc041d
u4bi <u4bi@u4bi.com>
2017-02-09 23:47:31 +0900

    프로젝트 환경 : 파이어베이스 설정정보 등록 - environments 폴더내 firebase.config.ts 정보 등록

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBwpOmhMEhMkx1AFLZjikdEsL89uz4dYx0",
+    authDomain: "overwatch-shotgame.firebaseapp.com",
+    databaseURL: "https://overwatch-shotgame.firebaseio.com",
+    storageBucket: "overwatch-shotgame.appspot.com",
+}
