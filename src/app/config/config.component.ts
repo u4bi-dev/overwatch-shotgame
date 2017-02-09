06b712a3299ac46896c56d2590a556b167c36e51
u4bi <u4bi@u4bi.com>
2017-02-09 12:22:36 +0900

    환경설정 뷰단 프로토타입 렌더 v.001

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfigComponent implements OnInit {
 
+  medals = [
+    {
+      name: '착한 훈장',
+      icon: 'face'
+    },
+    {
+      name: '일급 훈장',
+      icon: 'face'
+    },
+    {
+      name: '한조 50킬 훈장',
+      icon: 'face'
+    },
+    {
+      name: '시즌1 랭킹권 훈장',
+      icon: 'face'
+    }
+  ];
+
+
   constructor() { }
 
   ngOnInit() {
