c178d2ea64bca77495766e11509ca30e506f08d8
u4bi <u4bi@u4bi.com>
2017-02-11 05:30:43 +0900

    랭킹전적 단락 데이터 주입

--- src/app/rank/rank.component.ts
+++ src/app/rank/rank.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppFirebaseService } from '../providers/app-firebase.service';
+
 @Component({
   selector: 'app-rank',
   templateUrl: './rank.component.html',
@@ -7,60 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RankComponent implements OnInit {
 
-  users = [
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    },
-    {
-      name: '홍길동',
-      score: 42,
-      kill : 23
-    }
-  ];
+  playerRank : any = [];
 
-  constructor() { }
+  constructor(private appFirebaseService : AppFirebaseService) {
+    let self = this;
+
+    this.appFirebaseService.firebase.database.list('record').subscribe(
+      data =>{
+        data.forEach(function(item){
+          self.playerRank.push({
+            'name' : item.name,
+            'time' : item.time,
+            'kill' : item.kill
+          });
+          console.log(self.playerRank);
+        });
+      }
+    );
+  }
 
   ngOnInit() {
   }
